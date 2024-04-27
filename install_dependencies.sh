e69f544b2a235ffbd720263a4396f10ade8f925f
yohandiaz <yohan.diaz5632@gmail.com>
2024-04-27 16:53:31 +0200

    Refactor install_dependencies.sh script to check for existing VirtualBox and Vagrant installations before proceeding with installation

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -8,52 +8,65 @@ apt-get update -y
 echo "Installing package dependencies for the script..."
 apt-get install -y lsb-release wget gnupg2
 
-# Install VirtualBox
-echo "Installing VirtualBox..."
-
-# Importing Oracle public keys
-echo "Importing Oracle public keys..."
-wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | \
-  gpg --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg --dearmor
-
-# Adding VirtualBox APT Repository
-echo "Adding VirtualBox APT Repository..."
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" | \
-  tee -a /etc/apt/sources.list.d/virtualbox.list
-
-# Update system available packages
-echo "Updating available packages..."
-apt-get update -y
-
-# Install VirtualBox package
-echo "Installing VirtualBox package ..."
-apt-get install -y virtualbox-7.0 > /dev/null
-
-# Check for errors in installation
-if [ $? -eq 0 ]; then
-    echo "VirtualBox installed successfully!"
+# Check virtualbox isn't already installed
+if [ -x "$(command -v virtualbox)" ]; then
+    echo "VirtualBox is already installed. Skipping installation..."
+    exit 0
 else
-    echo "Failed to install VirtualBox. Check for errors above."
+    echo "VirtualBox is not installed. Proceeding with installation..."
+    # Install VirtualBox
+    echo "Installing VirtualBox..."
+
+    # Importing Oracle public keys
+    echo "Importing Oracle public keys..."
+    wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | \
+    gpg --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg --dearmor
+
+    # Adding VirtualBox APT Repository
+    echo "Adding VirtualBox APT Repository..."
+    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" | \
+    tee -a /etc/apt/sources.list.d/virtualbox.list
+
+    # Update system available packages
+    echo "Updating available packages..."
+    apt-get update -y
+
+    # Install VirtualBox package
+    echo "Installing VirtualBox package ..."
+    apt-get install -y virtualbox-7.0 > /dev/null
+
+    # Check for errors in installation
+    if [ $? -eq 0 ]; then
+        echo "VirtualBox installed successfully!"
+    else
+        echo "Failed to install VirtualBox. Check for errors above."
+    fi
 fi
 
-# Install Vagrant
-echo "Installing Vagrant..."
-
-# Importing HashiCorp public keys
-echo "Importing HashiCorp public keys..."
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-
-# Adding HashiCorp APT Repository
-echo "Adding HashiCorp APT Repository..."
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-
-# Installing vagrant package
-echo "Installing Vagrant package..."
-sudo apt update && sudo apt install vagrant > /dev/null
-
-# Check for errors in installation
-if [ $? -eq 0 ]; then
-    echo "Vagrant installed successfully!"
+# Check vagrant isn't already installed
+if [ -x "$(command -v vagrant)" ]; then
+    echo "Vagrant is already installed. Skipping installation..."
+    exit 0
 else
-    echo "Failed to install Vagrant. Check for errors above."
-fi
+    # Install Vagrant
+    echo "Installing Vagrant..."
+
+    # Importing HashiCorp public keys
+    echo "Importing HashiCorp public keys..."
+    wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+    # Adding HashiCorp APT Repository
+    echo "Adding HashiCorp APT Repository..."
+    echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+    # Installing vagrant package
+    echo "Installing Vagrant package..."
+    sudo apt update && sudo apt install vagrant > /dev/null
+
+    # Check for errors in installation
+    if [ $? -eq 0 ]; then
+        echo "Vagrant installed successfully!"
+    else
+        echo "Failed to install Vagrant. Check for errors above."
+    fi
+    fi
